1b984046fa84c6e9e713f4f31ccc6e9b75584420
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-12-06 08:54:40 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile('./src/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
